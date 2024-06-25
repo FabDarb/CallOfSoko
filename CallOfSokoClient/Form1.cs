3c08765f0de01c81fc2fe68c90f4f6e6f84b785b
cp-22fad <cp-22fad@PC-BD21-16>
2024-06-25 08:35:11 +0200

    Add project files.

---
+++ CallOfSokoClient/Form1.cs
@@ -0,0 +1,10 @@
+namespace CallOfSokoClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ CallOfSokoHub/Hubs/MainHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace CallOfSokoHub
+{
+    public class MainHub : Hub
+    {
+
+    }
+}
