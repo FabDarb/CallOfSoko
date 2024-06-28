31a7cac3f3dc79075d32d4fab4f355bdcbb5e1ba
FabDarb <Fabien.Darbellay@ceff.ch>
2024-06-28 08:14:44 +0200

    add ellipse for shooting

--- CallOfSokoClient/Class/BackEnd/Player.cs
+++ CallOfSokoClient/Class/BackEnd/Player.cs
@@ -1,17 +1,54 @@
 ﻿using CallOfLibrary;
-
 namespace CallOfSokoClient.Class.BackEnd
 {
     public class Player : Block
     {
         public int Id { get; set; }
-        public Player(int x, int y, int id)
+
+        public Rectangle? MoveViewPlayer;
+
+        public int? RecX { get; set; }
+
+        public int? RecY { get; set; }
+
+        private int _X;
+
+        public new int X
+        {
+            set
+            {
+                if (RecX != null)
+                {
+                    RecX = _X - 50;
+                }
+                _X = value;
+            }
+            get { return _X; }
+        }
+        private int _Y;
+        public new int Y
+        {
+            set
+            {
+                if (RecY != null)
+                {
+                    RecY = _Y - 50;
+                }
+                _Y = value;
+            }
+            get { return _Y; }
+        }
+        public Player(int x, int y, int id, int myUserId)
         {
             X = x;
             Y = y;
             Id = id;
             HitBox = new Rectangle(X, Y, 25, 25);
             Type = DataBlockType.Player;
+            if (myUserId == id)
+            {
+                MoveViewPlayer = new Rectangle(RecX!.Value, RecY!.Value - 50, 125, 125);
+            }
         }
 
         public override void DrawBlock(PaintEventArgs e, PictureBox display)
@@ -26,6 +63,10 @@ namespace CallOfSokoClient.Class.BackEnd
                 brush = Brushes.Indigo;
             }
             e.Graphics.FillRectangle(brush, X, Y, 25, 25);
+            if (MoveViewPlayer != null)
+            {
+                e.Graphics.FillEllipse(Brushes.LightGray, MoveViewPlayer.Value);
+            }
         }
     }
 }
