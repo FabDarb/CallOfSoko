8ee0ce91d7c02bac1900db09b3cf4b840f225007
FabDarb <Fabien.Darbellay@ceff.ch>
2024-06-28 11:29:40 +0200

    add bullet

---
+++ CallOfSokoClient/Class/BackEnd/Ball.cs
@@ -0,0 +1,24 @@
+﻿
+namespace CallOfSokoClient.Class.BackEnd
+{
+    public class Ball : Block
+    {
+        public int IdPlayer { get; set; }
+        public int Damage { get; set; } = 10;
+
+        public int Speed { get; set; } = 10;
+
+        public Ball(int x, int y, int id)
+        {
+            IdPlayer = id;
+            X = x;
+            Y = y;
+            HitBox = new Rectangle(X, Y, 5, 5);
+        }
+
+        public override void DrawBlock(PaintEventArgs e, PictureBox display)
+        {
+            e.Graphics.FillEllipse(Brushes.DarkGray, X, Y, 5, 5);
+        }
+    }
+}
